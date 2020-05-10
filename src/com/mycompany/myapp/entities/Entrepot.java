e3cf23dd1c0b4a60f5f050f08c04e496149af332
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-05-10 01:00:16 +0100

    entite

--- src/com/mycompany/myapp/entities/Entrepot.java
+++ src/com/mycompany/myapp/entities/Entrepot.java
@@ -10,5 +10,96 @@ package com.mycompany.myapp.entities;
  * @author asus
  */
 public class Entrepot {
+       int id_entrepot;
+    String adresse_entrepot;
+    int num_fiscale;
+    int quantite_max;
+    String etat;
+    String entreprise;
+    float prix_location;
+    int fk_id_fournisseur;
+
+    public Entrepot() {
+    }
+
+    public Entrepot(int id_entrepot, String adresse_entrepot, int num_fiscale, int quantite_max, String etat, String entreprise, float prix_location, int fk_id_fournisseur) {
+        this.id_entrepot = id_entrepot;
+        this.adresse_entrepot = adresse_entrepot;
+        this.num_fiscale = num_fiscale;
+        this.quantite_max = quantite_max;
+        this.etat = etat;
+        this.entreprise = entreprise;
+        this.prix_location = prix_location;
+        this.fk_id_fournisseur = fk_id_fournisseur;
+    }
+
+    public int getId_entrepot() {
+        return id_entrepot;
+    }
+
+    public void setId_entrepot(int id_entrepot) {
+        this.id_entrepot = id_entrepot;
+    }
+
+    public String getAdresse_entrepot() {
+        return adresse_entrepot;
+    }
+
+    public void setAdresse_entrepot(String adresse_entrepot) {
+        this.adresse_entrepot = adresse_entrepot;
+    }
+
+    public int getNum_fiscale() {
+        return num_fiscale;
+    }
+
+    public void setNum_fiscale(int num_fiscale) {
+        this.num_fiscale = num_fiscale;
+    }
+
+    public int getQuantite_max() {
+        return quantite_max;
+    }
+
+    public void setQuantite_max(int quantite_max) {
+        this.quantite_max = quantite_max;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    public String getEntreprise() {
+        return entreprise;
+    }
+
+    public void setEntreprise(String entreprise) {
+        this.entreprise = entreprise;
+    }
+
+    public float getPrix_location() {
+        return prix_location;
+    }
+
+    public void setPrix_location(float prix_location) {
+        this.prix_location = prix_location;
+    }
+
+    public int getFk_id_fournisseur() {
+        return fk_id_fournisseur;
+    }
+
+    public void setFk_id_fournisseur(int fk_id_fournisseur) {
+        this.fk_id_fournisseur = fk_id_fournisseur;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrepot{" + "id_entrepot=" + id_entrepot + ", adresse_entrepot=" + adresse_entrepot + ", num_fiscale=" + num_fiscale + ", quantite_max=" + quantite_max + ", etat=" + etat + ", entreprise=" + entreprise + ", prix_location=" + prix_location + ", fk_id_fournisseur=" + fk_id_fournisseur + '}';
+    }
     
 }
