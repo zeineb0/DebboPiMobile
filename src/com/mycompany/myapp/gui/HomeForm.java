ef99866e955da1685b62fcdbc9392b241e1b10ca
TaherZ <taher.zoghlami1b@gmail.com>
2020-05-13 00:56:38 +0100

    Update HomeForm.java

--- src/com/mycompany/myapp/gui/HomeForm.java
+++ src/com/mycompany/myapp/gui/HomeForm.java
@@ -5,10 +5,24 @@
  */
 package com.mycompany.myapp.gui;
 
+import com.codename1.ui.Button;
+import com.codename1.ui.Form;
+import com.codename1.ui.layouts.BoxLayout;
+
 /**
  *
  * @author Zeineb_yahiaoui
  */
-public class HomeForm {
-    
+public class HomeForm extends Form {
+    Form current;
+    public HomeForm(){
+        current=this;
+        setTitle("Home");
+        setLayout(BoxLayout.y());
+        Button btnList = new Button("List Employées");
+        btnList.addActionListener( e -> new ListEmployeForm(current).show());
+        add(btnList);
+        
+    }
+     
 }
