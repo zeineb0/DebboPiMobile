854c8f431d3e412b59332761dec315c7063db7e4
TaherZ <taher.zoghlami1b@gmail.com>
2020-05-14 13:15:57 +0100

    Create conge.java

---
+++ src/com/mycompany/myapp/entities/conge.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.myapp.entities;
+
+import java.util.Date;
+
+/**
+ *
+ * @author ASUS X550V
+ */
+public class conge {
+    int id;
+    Date datesortie;
+    Date datearrive;
+    String type;
+    String etat;
+    int FK_id_emp;
+    
+    
+}
