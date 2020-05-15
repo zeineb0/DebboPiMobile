56e3bab5ba5077a4dc1158a8277ef4c17bdbf080
GhaziHachicha <60756945+GhaziHachicha@users.noreply.github.com>
2020-05-15 07:54:08 +0200

    UpdatePanier

--- src/com/mycompany/myapp/entities/Panier.java
+++ src/com/mycompany/myapp/entities/Panier.java
@@ -5,10 +5,27 @@
  */
 package com.mycompany.myapp.entities;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author ghazi
  */
 public class Panier {
+    
+    int idUser;
+    double total;
+    ArrayList<ProduitCommande> produitCommande;
+    int nbrProduit;
+
+    public Panier(int idUser) {
+        this.idUser = idUser;
+        this.total = total=0;
+        this.produitCommande = new ArrayList<>();
+        this.nbrProduit = 0;
+    }
+    
+    
+    
     
 }
