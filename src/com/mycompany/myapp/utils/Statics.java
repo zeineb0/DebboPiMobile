17281a82ed4b352269f9b9862d5a3511339a4f65
GhaziHachicha <60756945+GhaziHachicha@users.noreply.github.com>
2020-05-15 07:56:33 +0200

    Update Statics

--- src/com/mycompany/myapp/utils/Statics.java
+++ src/com/mycompany/myapp/utils/Statics.java
@@ -6,6 +6,8 @@
 package com.mycompany.myapp.utils;
 
 import com.codename1.l10n.SimpleDateFormat;
+import com.mycompany.myapp.entities.Panier;
+import java.util.ArrayList;
 
 /**
  *
@@ -14,9 +16,10 @@ import com.codename1.l10n.SimpleDateFormat;
 public class Statics {
 
     public static final String GHAZI_URL="http://localhost/DebboPiWeb/web/app_dev.php/Mobile";
-     
     public static final String pattern = "HH:mm MM-dd-yyyy";
     public static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
     public static final String BASE_URL="http://localhost/DebboWeb/web/app_dev.php/Stock";
+    
+    public static final ArrayList<Panier> listPanier = new ArrayList<>();
     
 }
