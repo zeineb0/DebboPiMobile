728f4d55a697f5bfd4f2f523a35b9153c9e409c6
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-05-24 20:02:58 +0200

    update_mail_api

--- src/com/mycompany/myapp/transporteur/gui/HomeContratFrom.java
+++ src/com/mycompany/myapp/transporteur/gui/HomeContratFrom.java
@@ -5,10 +5,17 @@
  */
 package com.mycompany.myapp.transporteur.gui;
 
+
+import com.codename1.messaging.Message;
 import com.codename1.ui.Button;
 import com.codename1.ui.Form;
 import com.codename1.ui.Label;
+import com.codename1.ui.events.ActionEvent;
+import com.codename1.ui.events.ActionListener;
 import com.codename1.ui.layouts.BoxLayout;
+
+
+
 
 /**
  *
@@ -28,11 +35,21 @@ public class HomeContratFrom extends Form{
         Button btnAddContrat = new Button("Ajouter Contrat");
         Button btnListContrat = new Button("La Liste des Contrats");
         Button btnListContratEXP = new Button("La Liste des Contrats bientôt expirer");
+        Button sendMail= new Button("send mail");
+        
+        sendMail.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                Message msg = new Message("hello Test mail");
+                Message.sendMessage(new String[]{"medfarouk.benakacha@gmail.com"}, "Mail", msg);
+            }
+        });
+        
         
        // btnAddContrat.addActionListener(e);
         btnListContrat.addActionListener(e->new ListeContrat(current).show());
         btnListContratEXP.addActionListener(e-> new ListeContratEXP(current).show());
-        addAll(btnAddContrat,btnListContrat,btnListContratEXP);
+        addAll(btnAddContrat,btnListContrat,btnListContratEXP,sendMail);
      }
     
     
